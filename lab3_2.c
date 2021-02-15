5b647e0fa3fc8fa8feaef4252295932b9d106e14
Paphatsinee <paphatsinee.r@ku.th>
2021-02-15 19:22:58 +0700

    Update lab3_2.c

--- lab3_2.c
+++ lab3_2.c
@@ -5,6 +5,10 @@ int main()
     scanf("%d",&n);
     if(n>=1 && n<=10000)
     {
-
+        int a[n],b[n],i;
+        for(i=0;i<n;i++)
+        {
+            scanf("%d %d",&a[i],&b[i]);
+        }
     }
 }
