dbff9f353888e2af452562112f3d682f538d4aac
Paphatsinee <paphatsinee.r@ku.th>
2021-02-15 17:11:20 +0700

    Update lab3_1.c

--- lab3_1.c
+++ lab3_1.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int a[5][4],i,j,max=0,win=0;
-    for(i=0;i<5;i++)
+    int a=0,i,j,max=0,win=0;
+    for(i=1;i<=5;i++)
     {
         for(j=0;j<4;j++)
         {
             scanf("%d",&a[i][j]);
+
         }
         if(a[i][j]>max)
         {
-            max=a[i][j];
+            max=a[j];
             win=i;
         }
+
     }
     printf("%d %d",win,max);
 
