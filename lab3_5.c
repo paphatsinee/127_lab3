e1cb4fc728f6d0ad5e76141f312164764d31cb12
Paphatsinee <paphatsinee.r@ku.th>
2021-02-15 19:52:55 +0700

    Update lab3_5.c

--- lab3_5.c
+++ lab3_5.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int num,a;
+    int num,a,b=2,sum,c;
     scanf("%d",&num);
     if(0<num<10000)
     {
-        a=num,b=2;
+        a=num;
         while(a!=1)
         {
             while(a%b==0)
@@ -13,9 +13,11 @@ int main()
                 a/=b;
                 if(a%b==0)
                 {
-
+                    sum++;
                 }
+                c++;
             }
+            b++;
         }
     }
 }
