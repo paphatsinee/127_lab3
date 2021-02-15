150319671d71af3d2c89931959b29b88665e2382
Paphatsinee <paphatsinee.r@ku.th>
2021-02-15 18:36:19 +0700

    Update lab3_4.c

--- lab3_4.c
+++ lab3_4.c
@@ -3,41 +3,45 @@ int main()
 {
     int n,a,b;
     scanf("%d",&n);
-    if(n%2==0)
+    if(n%2==0 || n%3==0)
     {
-        while(n%2==0)
+        if(n%2==0)
         {
-            a=n-1;
-            n=n-1;
-        }
-        while(n%2==0)
-        {
-            b=n+1;
-            n=n+1;
-        }
-        if(a>b)
-            a=b;
+            while(n%2==0)
+            {
+                a=n-1;
+                n=n-1;
+            }
+            while(n%2==0)
+            {
+                b=n+1;
+                n=n+1;
+            }
+            if(a>b)
+                a=b;
 
-    }
-    else if(n%3==0)
-    {
-        while(n%3==0)
-        {
-            a=n-1;
-            n=n-1;
         }
-        while(n%3==0)
+        else if(n%3==0)
         {
-            b=n+1;
-            n=n+1;
+            while(n%3==0)
+            {
+                a=n-1;
+                n=n-1;
+            }
+            while(n%3==0)
+            {
+                b=n+1;
+                n=n+1;
+            }
+            if(a>b)
+                a=b;
         }
-        if(a>b)
-            a=b;
+        printf("%d",a);
     }
     else printf("%d",n);
 
 
-    printf("%d",a);
+
 
     return 0;
 }
