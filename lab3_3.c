a648603ece46f1b55e7cd4a26af50a839b91e5be
Paphatsinee <paphatsinee.r@ku.th>
2021-02-15 17:31:47 +0700

    Update lab3_3.c

--- lab3_3.c
+++ lab3_3.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int k;
+    int k,sum=1,i;
     scanf("%d",&k);
     if(1 <= k <= 1000)
     {
+        for(i=1;i<=k;i++)
+            sum *= i;
 
+        printf("%d",sum);
     }
+
+    return 0;
 }
