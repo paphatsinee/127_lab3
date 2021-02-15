31cf9d61ff94e09d70d78512722ef3d47e8c2e9b
Paphatsinee <paphatsinee.r@ku.th>
2021-02-15 19:51:07 +0700

    Update lab3_5.c

--- lab3_5.c
+++ lab3_5.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int num;
+    int num,a;
     scanf("%d",&num);
     if(0<num<10000)
     {
+        a=num,b=2;
+        while(a!=1)
+        {
+            while(a%b==0)
+            {
+                a/=b;
+                if(a%b==0)
+                {
 
+                }
+            }
+        }
     }
 }
